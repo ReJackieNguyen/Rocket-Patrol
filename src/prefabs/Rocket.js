d88e8b647f53dea5f55fabe8fcd480f9c4631210
Jackie Nguyen <jahunguy@ucsc.edu>
2021-06-25 15:45:18 -0700

    Fast

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -3,12 +3,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
       super(scene, x, y, texture, frame);
   
-      // add object to existing scene
-      scene.add.existing(this);
-      //track rocket's firing status
-      this.isFiring = false;
-      //pixels per frame
-      this.moveSpeed = 2;
+
+      scene.add.existing(this);     // add object to existing scene     
+      this.isFiring = false;        // track rocket's firing status
+      this.moveSpeed = 2;           // pixels per frame
     }
 
     update() {
@@ -25,18 +23,20 @@ class Rocket extends Phaser.GameObjects.Sprite {
         if (Phaser.Input.Keyboard.JustDown(keyF)) {
             this.isFiring = true;
         }
-        if (Phaser.Input.Keyboard.JustDown(keyF)) {
-            this.isFiring = true;
-        }
         // if fired, move up
         if (this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
             this.y -= this.moveSpeed;
         }
         // reset on miss
         if (this.y <= borderUISize * 3 + borderPadding) {
-            this.isFiring = false;
-            this.y = game.config.height - borderUISize - borderPadding;
+            this.reset();
         }
 
+    }
+
+    // reset rocket to "ground"
+    reset () {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
 }

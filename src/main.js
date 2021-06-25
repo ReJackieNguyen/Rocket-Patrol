61593147165f9762aa40cef8d505452800b0e6f9
Jackie Nguyen <jahunguy@ucsc.edu>
2021-06-25 16:27:06 -0700

    Almost done

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
-    scene: [ Menu, Play ]
+    scene: [ Menu, Play ],
+    fps: {
+      target: 30, forceSetTimeOut: true
+
+    }
   }
 
 let game = new Phaser.Game(config);
